124eec29e417433e43b3e89098bbe4505afbf49e
mikeusa04 <71799572+mikeusa04@users.noreply.github.com>
2020-12-16 12:09:02 -0800

    week5 assignment

---
+++ fsw-110/week5/pauseAndPractice1/index.js
@@ -0,0 +1,8 @@
+function getdetails(){
+var a = document.forms["details"]["lastname"].value;
+var b = document.forms["details"]["firstname"].value;
+var c= document.forms["details"]["email"].value;
+alert("Your name is: "+a+" "+b+". Your e-mail is: "+c);
+}
+
+//window.alert("hiiii")
